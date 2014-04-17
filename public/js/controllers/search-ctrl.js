fc1a56599a90691902f56e5b7eda6130f68573f3
Jake Marsh <jakemmarsh@gmail.com>
2014-04-17 19:28:06 -0400

    added an 'analyzing' flag to show a loading screen on the frontend while the neural network is training/working

--- public/js/controllers/search-ctrl.js
+++ public/js/controllers/search-ctrl.js
@@ -5,10 +5,13 @@ define(['./index'], function (controllers) {
         $scope.analyzed = false;
 
         $scope.analyzeSymbol = function(symbol) {
+            $scope.analyzing = true;
             apiService.analyzeSymbol(symbol).then(function(data) {
                 $scope.analysisData = data;
+                $scope.analyzing = false;
                 $scope.analyzed = true;
             }, function(err) {
+                $scope.analyzing = false;
                 $scope.analyzed = false;
                 $scope.errorMessage = err;
             });
--- public/js/templates.js
+++ public/js/templates.js
@@ -9,14 +9,19 @@ define(['angular', 'app'], function(angular, app) { app.run(['$templateCache', f
     "<div style=\"margin-top: 5%;\" ng-hide=\"analyzed\">\n" +
     "    <h2>Analyze a Stock Symbol</h2>\n" +
     "    <div class=\"well\">\n" +
-    "        <p>Search for a stock symbol to attempt to analyze using the neural network.</p>\n" +
-    "        <form name=\"symbolSearch\" class=\"input-group input-group-lg\" ng-submit=\"analyzeSymbol(symbol)\">\n" +
-    "            <input type=\"text\" class=\"form-control\" id=\"symbol\" name=\"symbol\" ng-model=\"symbol\" required />\n" +
-    "            <span class=\"input-group-btn\">\n" +
-    "                <button class=\"btn btn-primary\" type=\"submit\" ng-disabled=\"symbolSearch.$invalid\">Analyze</button>\n" +
-    "            </span>\n" +
-    "        </form>\n" +
-    "        <strong class=\"text-danger\" style=\"margin-top: 8px; display: block;\" ng-bind=\"errorMessage\"></strong>\n" +
+    "        <div ng-hide=\"analyzing\">\n" +
+    "            <p>Search for a stock symbol to attempt to analyze using the neural network.</p>\n" +
+    "            <form name=\"symbolSearch\" class=\"input-group input-group-lg\" ng-submit=\"analyzeSymbol(symbol)\">\n" +
+    "                <input type=\"text\" class=\"form-control\" id=\"symbol\" name=\"symbol\" ng-model=\"symbol\" required />\n" +
+    "                <span class=\"input-group-btn\">\n" +
+    "                    <button class=\"btn btn-primary\" type=\"submit\" ng-disabled=\"symbolSearch.$invalid\">Analyze</button>\n" +
+    "                </span>\n" +
+    "            </form>\n" +
+    "            <strong class=\"text-danger\" style=\"margin-top: 8px; display: block;\" ng-bind=\"errorMessage\"></strong>\n" +
+    "        </div>\n" +
+    "        <div ng-show=\"analyzing\">\n" +
+    "            <p>Analyzing <span ng-bind=\"symbol\"></span>...</p>\n" +
+    "        </div>\n" +
     "    </div>\n" +
     "</div>\n" +
     "\n" +
