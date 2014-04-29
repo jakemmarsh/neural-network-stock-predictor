5b4e70a139b71af168e1750b957079af1318e8b3
Jake Marsh <jakemmarsh@gmail.com>
2014-04-28 22:14:58 -0400

    started logic to provide rolling windows of average, minimum, and maximum for training neural network

--- analyzer.py
+++ analyzer.py
@@ -4,6 +4,63 @@ from time import mktime
 
 from neuralNetwork import NeuralNetwork
 
+## ================================================================
+
+def rollingWindow(seq, windowSize):
+    it = iter(seq)
+    win = [it.next() for cnt in xrange(windowSize)] # First window
+    yield win
+    for e in it: # Subsequent windows
+        win[:-1] = win[1:]
+        win[-1] = e
+        yield win
+
+def getMovingAverage(values, windowSize):
+    movingAverages = []
+    
+    for w in rollingWindow(values, windowSize):
+        movingAverages.append(sum(w)/len(w))
+
+    return movingAverages
+
+def getMinimums(values, windowSize):
+    minimums = []
+
+    for w in rollingWindow(values, windowSize):
+        minimums.append(min(w))
+            
+    return minimums
+
+def getMaximums(values, windowSize):
+    maximums = []
+
+    for w in rollingWindow(values, windowSize):
+        maximums.append(max(w))
+
+    return maximums
+
+## ================================================================
+
+def getTimeSeriesValues(values, window):
+    print "values:", values
+    movingAverages = getMovingAverage(values, window)
+    minimums = getMinimums(values, window)
+    maximums = getMaximums(values, window)
+
+    returnData = []
+
+    # build items of the form [[average, minimum, maximum], normalized price]
+    for i in range(0, len(movingAverages) - 1):
+        inputNode = [movingAverages[i], minimums[i], maximums[i]]
+        price = normalizePrice(values[len(movingAverages) - (i + 1)], minimums[i], maximums[i])
+        outputNode = [price]
+        tempItem = [inputNode, outputNode]
+        returnData.append(tempItem)
+
+    return returnData
+
+## ================================================================
+
 def getHistoricalData(stockSymbol):
     historicalPrices = []
     
@@ -11,31 +68,60 @@ def getHistoricalData(stockSymbol):
     urllib2.urlopen("http://api.kibot.com/?action=login&user=guest&password=guest")
 
     # get 10 days of data from API (business days only, could be < 10)
-    url = "http://api.kibot.com/?action=history&symbol=" + stockSymbol + "&interval=daily&period=10&unadjusted=1&regularsession=1"
+    url = "http://api.kibot.com/?action=history&symbol=" + stockSymbol + "&interval=daily&period=14&unadjusted=1&regularsession=1"
     apiData = urllib2.urlopen(url).read().split("\n")
-
-    # get price for each day returned from API
     for line in apiData:
         if(len(line) > 0):
             tempLine = line.split(',')
-            utcdate = mktime(datetime.utctimetuple(datetime.strptime(tempLine[0], "%m/%d/%Y")))
             price = float(tempLine[1])
-            historicalPrices.append([utcdate, price])
+            historicalPrices.append(price)
 
     return historicalPrices
 
-def analyzeSymbol(stockSymbol):
-    stockPrices = getHistoricalData(stockSymbol)
-    network = NeuralNetwork(inputNodes = 2, hiddenNodes = 2, outputNodes = 1)
+## ================================================================
 
-    # train neural network with historical prices
+def getTrainingData(stockSymbol):
+    historicalData = getHistoricalData(stockSymbol)
+
+    # ensure we only have 9 data points, then reverse it so we're using the most recent data first
+    del historicalData[9:]
+    historicalData.reverse()
+
+    # get five 5-day moving averages, 5-day lows, and 5-day highs, associated with the closing price
+    trainingData = getTimeSeriesValues(historicalData, 5)
+
+    return trainingData
+
+## ================================================================
+
+## ================================================================
+
+def normalizePrice(price, minimum, maximum):
+    return ((2*price - (maximum + minimum)) / (maximum - minimum))
+
+def denormalizePrice(price, minimum, maximum):
+    return (((price*(maximum-minimum))/2) + (maximum + minimum))/2
+
+## ================================================================
+
+def analyzeSymbol(stockSymbol):
+    trainingData = getTrainingData(stockSymbol)
+    
+    network = NeuralNetwork(inputNodes = 3, hiddenNodes = 3, outputNodes = 1)
+
+    print trainingData
+
+    network.train(trainingData)
+
+    # TODO: GET ROLLING DATA FOR TOMORROW (NEXT BUSINESS DAY)
+    predictionData = [530.222, 522.51, 536.1]
 
     # get prediction
+    returnPrice = network.test(predictionData)
 
-    # create array of format [nextBusinessDayUTC, predictedPrice]
-    # push array onto historicalPrices
+    # de-normalize and return predicted stock price
+    predictedStockPrice = denormalizePrice(returnPrice, predictionData[1], predictionData[2])
 
-    # return prices, along with prediction
-    return stockPrices
+    return predictedStockPrice
 
-print getHistoricalData("GOOG")
+print analyzeSymbol("GOOG")
